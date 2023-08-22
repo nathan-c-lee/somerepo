61588a4343d593fcbced34f5995f2b4eccb8e22b
nathan-c-lee <97199040+nathan-c-lee@users.noreply.github.com>
2023-08-22 02:55:18 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,10 @@
 
+
+def ask_name():
+    mn = input("what is your name")
+  if mn.isalpha():
+    print(f'hello {mn}')
+    return False, mn
+  else:
+    print("error, bad input")
+    return True
