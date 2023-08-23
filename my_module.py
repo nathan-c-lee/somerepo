f2d2fe7ccfe9e5ebf5b4dc84eba84a3ad03f64d5
nathan-c-lee <97199040+nathan-c-lee@users.noreply.github.com>
2023-08-23 16:37:35 -0400

    Update my_module.py

--- my_module.py
+++ my_module.py
@@ -3,8 +3,8 @@
 def ask_name():
   mn = input(">> what is your name?: ")
   if mn.isalpha():
-    print(f'hello {mn}, goodbye {mn}', "")
+    print(f'hello {mn}, goodbye {mn}', "\n")
     return False
   else:
-    print("error, bad input", "")
+    print("error, bad input", "\n")
     return True
