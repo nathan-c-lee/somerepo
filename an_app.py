abf417833961fcb018e9e18b3b6434f7116f12b6
nathan-c-lee <nathan.christopher.lee@gmail.com>
2023-08-23 16:36:17 -0400

    app update

--- an_app.py
+++ an_app.py
@@ -5,13 +5,12 @@ while True:
 	with open("a_file.txt", "r") as file:
 	
 		for line in file:
-			if "//" in line:
-				while ask_name():
-					continue
-			else:
-				print(line)
+			print(line)
+	
+	while ask_name():
+		continue
 
-	q = input("type 'q' to quit: ").lower()
+	q = input("\ntype 'q' to quit: ").lower()
 	if q == "q":
 		break
 	else:
